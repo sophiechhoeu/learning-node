595f5a483d4af6c1fc9380876b8ce6942f4b61cc
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2018-01-04 02:53:54 +1100

    additional notes: how mongoose works to save to mongo DB

--- app/server.js
+++ app/server.js
@@ -19,16 +19,30 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 var dbUrl = 'mongodb://user:user@ds239117.mlab.com:39117/learning-node'
 
+//message model for mongoose
+// passing schema with the properties and type
 var Message = mongoose.model('Message', {
     name: String,
     message: String
 })
+
+//instead of sending an array we'll send messages from mongoDB
+//message.find selecting all the messages passing no requirements through
+//create error call back
+//include all messages in the call back
 
 app.get('/messages', (req, res) => {
     Message.find({}, (err, messages) =>{
         res.send(messages)
     })
 })
+
+//creating an object based on our new mongoose model
+//pass the req.body because that's where our message is contained
+//message.save to mongo DB
+//if successful message save to the mongoDB then it will emit a socketIO EVENT
+//it will send a status of 200
+//if it errors it'll send a status of 500.
 
 app.post('/messages', (req, res) => {
     var message = new Message(req.body)
