57433377858d256edbdc268ff1e5737dce70dcdd
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2018-01-04 17:34:27 +1100

    completed jasmine tests

--- app/spec/server.spec.js
+++ app/spec/server.spec.js
@@ -7,7 +7,7 @@ describe('calc', () => {
 })
 
 //calling done whenever our aysncyhronous code finishes
-
+//body.length (of a string so testing the characters of a string)
 describe('get messages', () => {
   it('should return 200 ok', (done) => {
     request.get('http://localhost:3000/messages', (err, res) => {
@@ -17,8 +17,23 @@ describe('get messages', () => {
   })
   it('should return list, thats not empty', (done) => {
     request.get('http://localhost:3000/messages', (err, res) => {
-      expect(res.body.length).toBeGreaterThan(0)
+      expect(JSON.parse(res.body).length).toBeGreaterThan(0)
       done()
+    })
+  })
+})
+
+describe('get messages from user', () => {
+  it('should return 200 ok', (done) => {
+    request.get('http://localhost:3000/messages/des', (err, res) => {
+      expect(res.statusCode).toEqual(200)
+      done()
+    })
+  })
+    it('name should be des', (done) => {
+      request.get('http://localhost:3000/messages/des', (err, res) => {
+        expect(JSON.parse(res.body)[0].name).toEqual('des')
+        done()
     })
   })
 })
