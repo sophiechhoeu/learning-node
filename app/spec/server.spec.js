ea5fa7030e51e023fcafe5c920716e30d498105a
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2018-01-04 16:18:12 +1100

    added get messages tests for jasmine, checking length of messages list

--- app/spec/server.spec.js
+++ app/spec/server.spec.js
@@ -1,5 +1,24 @@
+var request = require('request')
+
 describe('calc', () => {
   it('should multiply 2 and 2', () => {
     expect (2*2).toBe(4)
+  })
+})
+
+//calling done whenever our aysncyhronous code finishes
+
+describe('get messages', () => {
+  it('should return 200 ok', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) => {
+      expect(res.statusCode).toEqual(200)
+      done()
+    })
+  })
+  it('should return list, thats not empty', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) => {
+      expect(res.body.length).toBeGreaterThan(0)
+      done()
+    })
   })
 })
